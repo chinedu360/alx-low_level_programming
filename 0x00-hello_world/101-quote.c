4eee4a9ab2ac3f80960d3406a23cedd9711abeff
chinedu360 <okere360@gmail.com>
2022-09-08 21:45:37 -0700

    fix2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Description: prints and tha piece..
- * Return: Always 1 (Success)
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",followed by a new line, to standard error.
+ * Return: Always 1.
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
