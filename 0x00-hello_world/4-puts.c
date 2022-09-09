5da27a83aa4a5d2b51aaf4b8eb8d608569aad7bf
chinedu360 <okere360@gmail.com>
2022-09-08 18:23:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main- servers as the entry point for the program
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
