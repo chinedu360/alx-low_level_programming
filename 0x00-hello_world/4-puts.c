de7ce5279114a2dab0a864e4b72a54f0456097e3
chinedu360 <okere360@gmail.com>
2022-09-08 18:26:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
  * main- servers as the entry point for the program
+ *
+ * return: by default returns zero if no errors otherwise returns no zero value.
  */
 int main(void)
 {
