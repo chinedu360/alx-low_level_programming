ac3f71b4e7fae7f70dcc6dcc38659261d0550de8
chinedu360 <okere360@gmail.com>
2022-09-08 21:47:46 -0700

    fix 3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",followed by a new line, to standard error.
+ * main - Prints "prints a quote"
  * Return: Always 1.
  */
 int main(void)
