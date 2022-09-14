122cbcbc1c7efefcc52c765d54be4bdecdd7dbbb
chinedu360 <okere360@gmail.com>
2022-09-14 16:07:28 -0700

    add num

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.c"
+/**
+ * add - the funtion adds two ints and returns the val
+ * @one: the first num
+ * @two: the second num
+ * Return: one + two
+ */
+int add(int one, int two)
+{
+	return (one + two)
+}
