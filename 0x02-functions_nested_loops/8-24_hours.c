6d706456514921391a631ce79c4e36d27a463ee7
chinedu360 <okere360@gmail.com>
2022-09-13 21:31:42 -0700

    no quiting

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,9 +13,12 @@ void jack_bauer(void)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			_putchar((h / 10) + '0'):										_putchar((h % 10) + '0');
-			_putchar(':');												_putchar((m / 10) + '0');										_putchar((m % 10) + '0');
-			_putchar('\n');
+			_putchar((h / 10) + '0'):
+			_putchar((h % 10) + '0');
+			_putchar(':')
+			_putchar((m / 10) + '0');
+			_putchair((m % 10) + '0');
+		       	_putchar('\m');	
 		}
 	}
-}
+}	
