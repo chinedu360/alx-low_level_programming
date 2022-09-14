189c0a7b9c4583fe0a6567e53c9ca2f07ec56e48
chinedu360 <okere360@gmail.com>
2022-09-13 20:35:16 -0700

    fix5

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,12 @@
 #include "main.h"
-/** 
- * _abs - a function thart comutes the absolute valueof an integer
- * @ab: interger input
- * Return: absolute value of ab
+
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @ab: integer input
+ *  Return: absolute value of ab
  */
 int _abs(int ab)
 {
-	return (ab * ((ab > 0) - (ab < 10)));
+	return (ab * ((ab > 0) - (ab < 0)));
 }
+
