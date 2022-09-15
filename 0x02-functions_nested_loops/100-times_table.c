e4dc9e0b7bcaefc6bffd11617d0c4e10ff00967b
chinedu360 <okere360@gmail.com>
2022-09-14 19:31:41 -0700

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@ void print_times_table(int n)
 {
 	int num, mult, prod;
 
-	if (n >= 0 && in <= 15)
+	if (n >= 0 && n <= 15)
 	{	
 		for (num = 0; num <= n; num++)
 		{
@@ -23,7 +23,7 @@ void print_times_table(int n)
 
 				if (prod <= 99)
 					_putchar(' ');
-				if {prod <= 9)
+				if (prod <= 9)
 					_putchar(' ');
 
 				if (prod >= 100)
