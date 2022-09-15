3d06c9f4b773af9e09d284cd0ef73f9faf659690
chinedu360 <okere360@gmail.com>
2022-09-14 19:45:54 -0700

    natural 2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  * main - list natural number below 1024
- * 		thatt are multiple of 2 and 3
+ * thatt are multiple of 2 and 3
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	int i, sum = 0;
+
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
