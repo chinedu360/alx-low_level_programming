9f8c5c1b5378a4ed4cfbb0e9cec2a47191bd1484
chinedu360 <okere360@gmail.com>
2022-09-19 21:23:43 -0700

    str copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,9 +11,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	for (; src[i] != '\0'; i++)
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
